2c74e62b6a5eb10ab219a63c8a3ed41599b5aee3
Vlas Sokolov <vlas.sokolov@solita.fi>
2021-04-09 15:22:58 +0200

    first lazy version of the .py pipeline

--- pipelines/bitti/config.py
+++ pipelines/bitti/config.py
@@ -6,6 +6,11 @@ from sagemaker.workflow.parameters import (ParameterInteger, ParameterString,
 from sagemaker.workflow.steps import CacheConfig
 
 
+# FIXME: clearly, needs more work
+class SGConf:
+    pass
+
+
 def read_conf(cfg_file):
     """Reads config file, returns a dict with workflow parameters"""
 
@@ -13,12 +18,13 @@ def read_conf(cfg_file):
     #        three names from one present in the .ini file. That would
     #        likely need to have some snake case to PascalCase and back
     #        conversion hacks
-    cfg_dict = {}
+    sg_cfg = SGConf()
     config = ConfigParser(interpolation=ExtendedInterpolation())
     config.read_file(open(cfg_file))
 
     region = boto3.Session().region_name
-    cfg_dict['bucket'] = config['metadata'].get('bucket')
+    sg_cfg.bucket = config['metadata'].get('bucket')
+    sg_cfg.pipeline_name = config['metadata'].get('pipeline_name')
 
     # fetch the execution role and account id from Secrets Manager
     session = boto3.session.Session()
@@ -29,69 +35,69 @@ def read_conf(cfg_file):
 
     secret_role = config['secretsmanager'].get('secret_role')
     secret_account_id = config['secretsmanager'].get('secret_account_id')
-    cfg_dict['role'] = client_secrets_fetch(SecretId=secret_role)['SecretString']
+    sg_cfg.role = client_secrets_fetch(SecretId=secret_role)['SecretString']
     account_id = client_secrets_fetch(
             SecretId=secret_account_id)['SecretString']
 
     # will reuse the same cache config for all the steps
-    cfg_dict['cache_config'] = CacheConfig(
+    sg_cfg.cache_config = CacheConfig(
             enable_caching=config['metadata'].getboolean('cache_steps'),
             expire_after=config['metadata'].get('cache_expire_after'))
     # FIXME: resolve with pathlib!
-    cfg_dict['source_dir'] = config['metadata'].get('source_dir')
+    sg_cfg.source_dir = config['metadata'].get('source_dir')
 
     # start defining workflow parameters for sagemaker pipeline steps
     # first off, processing steps
-    cfg_dict['input_data'] = ParameterString(
+    sg_cfg.input_data = ParameterString(
         name='InputData',
         default_value=config['processing'].get('input_data'))
 
-    cfg_dict['processing_instance_count'] = ParameterInteger(
+    sg_cfg.processing_instance_count = ParameterInteger(
         name='ProcessingInstanceCount',
         default_value=config['processing'].getint('instance_count'))
 
-    cfg_dict['processing_instance_type'] = ParameterString(
+    sg_cfg.processing_instance_type = ParameterString(
         name='ProcessingInstanceType',
         default_value=config['processing'].get('instance_type'))
 
-    cfg_dict['processing_train_test_split'] = ParameterFloat(
+    sg_cfg.processing_train_test_split = ParameterFloat(
         name='TrainTestSplit',
         default_value=config['processing'].getfloat('train_test_split_fraction'))
 
-    cfg_dict['processing_turicreate_uri'] = ParameterString(
+    sg_cfg.processing_turicreate_uri = ParameterString(
         name='TuriCreateProcessingURI',
         default_value=config['processing'].get('image_uri_fmt').format(account_id))
 
     # control settings for the training job
-    cfg_dict['training_instance_count'] = ParameterInteger(
+    sg_cfg.training_instance_count = ParameterInteger(
         name='TrainingInstanceCount',
         default_value=config['training'].getint('instance_count'))
 
-    cfg_dict['training_instance_type'] = ParameterString(
+    sg_cfg.training_instance_type = ParameterString(
         name='TrainingInstanceType',
         default_value=config['training'].get('instance_type'))
 
-    cfg_dict['training_batch_size'] = ParameterInteger(
+    sg_cfg.training_batch_size = ParameterInteger(
         name='TrainingBatchSize',
         default_value=config['training'].getint('batch_size'))
 
-    cfg_dict['training_max_iterations'] = ParameterInteger(
+    sg_cfg.training_max_iterations = ParameterInteger(
         name='MaxIterations',
         default_value=config['training'].getint('max_iterations'))
 
-    cfg_dict['training_turicreate_uri'] = ParameterString(
+    sg_cfg.training_turicreate_uri = ParameterString(
         name='TuriCreateTrainingURI',
         default_value=config['training'].get('image_uri_fmt').format(account_id))
 
     # workflow parameters for model approval / rejection
-    cfg_dict['model_approval_status'] = ParameterString(
+    sg_cfg.model_approval_status = ParameterString(
         name='ModelApprovalStatus',
         default_value=config['evaluation'].get('approval_status'))
 
-    cfg_dict['model_approval_map_threshold'] = ParameterFloat(
+    sg_cfg.model_approval_map_threshold = ParameterFloat(
         name='ModelApprovalmAPThreshold',
         default_value=config['evaluation'].getfloat('approval_map_threshold'))
 
-    cfg_dict['model_package_group_name'] = config['metadata'].get(
+    sg_cfg.model_package_group_name = config['metadata'].get(
             'model_package_group_name')
-    return cfg_dict
+    return sg_cfg
