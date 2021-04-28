b37ac2583dad73a7a7c63930367fcc994efe5164
Vlas Sokolov <vlas.sokolov@solita.fi>
2021-04-28 16:33:07 +0200

    force strings for s3 uploader - boto doesn't like pathlib

--- pipelines/bitti/step_scripts/publish_to_api.py
+++ pipelines/bitti/step_scripts/publish_to_api.py
@@ -10,7 +10,7 @@ DATA_PATH = Path("/opt/ml/processing").resolve()
 def upload_mlmodel():
     """Upload the .mlmodel artifact to API-enabled S3 bucket"""
 
-    model_file = DATA_PATH/"mlmodel/bitti.mlmodel"
+    model_file = str(DATA_PATH/"mlmodel/bitti.mlmodel")
     datestring = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
     mlfilename = f'bitti-{datestring}.mlmodel'
     bucket_name = "magazine-monitor"
@@ -22,7 +22,7 @@ def upload_mlmodel():
 def upload_model_card():
     """Upload the model card to a public S3 bucket w. static hosting"""
 
-    model_card_file = DATA_PATH/"model_card/model_card.html"
+    model_card_file = str(DATA_PATH/"model_card/model_card.html")
     output_file_name = "model_card_latest.html"
     bucket_name = "aigamodelcards.com"
     s3 = boto3.resource('s3')
